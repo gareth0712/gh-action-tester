7d3e05f58e16fa5759fe8f6bae7defe894a67d4b
gareth0712 <gareth_0712@hotmail.com>
2024-01-03 11:04:43 +0800

    feat: further update the script to avoid backtick

--- .github/scripts/summary.js
+++ .github/scripts/summary.js
@@ -1,5 +1,6 @@
 module.exports = async ({ core, header, body }) => {
   await core.summary.addHeading(header, '2');
+  body.replace('`', '|');
   await core.summary.addRaw(body, true);
 
   await core.summary.write({ overwrite: true });
