82fccae5c9f1cf9250aa34e7b8acf43580f984c7
gareth0712 <gareth_0712@hotmail.com>
2024-01-03 11:21:17 +0800

    feat: test for simple github summary

--- .github/scripts/summary.js
+++ .github/scripts/summary.js
@@ -1,6 +1,5 @@
 module.exports = async ({ core, header, body }) => {
-  await core.summary.addHeading(header, '2');
-  await core.summary.addRaw(body, true);
+  await core.summary.addHeading('hihi', '2');
 
   await core.summary.write({ overwrite: true });
 };
